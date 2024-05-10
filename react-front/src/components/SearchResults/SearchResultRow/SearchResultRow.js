5ef0ecc8c77fff99513070a7f8aa79c03080484f
Kody Bloodworth <41092484+koblu@users.noreply.github.com>
2024-05-10 11:31:31 -0400

    Search Result More Visually Interesting

--- react-front/src/components/SearchResults/SearchResultRow/SearchResultRow.js
+++ react-front/src/components/SearchResults/SearchResultRow/SearchResultRow.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Paper, Typography} from '@mui/material';
+import {Paper, Box, Typography} from '@mui/material';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const SearchResultRow = (props) => { 
   const mailingAddress = props.result?.addresses?.find((address => address.address_purpose === "MAILING"));
@@ -7,10 +8,33 @@ const SearchResultRow = (props) => {
   const result = props.result;
   return (
   <Paper elevation={4} sx={{width: '30%', height: '100%', paddingTop: '50px', paddingBottom: '50px'}} onClick={() => {props.setSelected(result.npiNo)}}>
-    {result.name && <Typography>{result.name}</Typography>}
-    <Typography> {result.address_1}</Typography>
-    {result.address_2 && <Typography> {result.address_2}</Typography>}
-    <Typography>{result.city}, {result.state}</Typography>
+    <Box
+      sx={{display:"flex",
+        flexDirection: "row",
+        justifyContent:"space-evenly"
+      }}
+    >
+      <Box
+          sx={{display:"flex",
+            flexDirection: "column",
+            flexBasis: "80%"
+          }}
+          >
+          {result.name && <Typography>{result.name}</Typography>}
+          <Typography> {result.address_1}</Typography>
+          {result.address_2 && <Typography> {result.address_2}</Typography>}
+          <Typography>{result.city}, {result.state}</Typography>
+        </ Box>
+      <Box
+        sx={{display:"flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          flexBasis: "20%"
+        }}
+        >
+        <ArrowForwardIosIcon />
+      </Box>
+    </ Box>
   </ Paper>
 )};
 
