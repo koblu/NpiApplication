c13ff10e3f985dd56fe2689020ed1a4f944aba77
Kody Bloodworth <41092484+koblu@users.noreply.github.com>
2024-05-10 13:51:28 -0400

    Page Load Tests

--- react-front/src/App.test.js
+++ react-front/src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/NPPES NPI Database Search Tool/i);
+  expect(title).toBeInTheDocument();
+});
+
+test('renders search form', () => {
+  render(<App />);
+  const npiNumber = screen.getByLabelText(/NPI Number/i);
+  expect(npiNumber).toBeInTheDocument();
+  const firstName = screen.getByLabelText(/First Name/i);
+  expect(firstName).toBeInTheDocument();
+  const lastName = screen.getByLabelText(/Last Name/i);
+  expect(lastName).toBeInTheDocument();
+  const city = screen.getByLabelText(/City/i);
+  expect(city).toBeInTheDocument();
+  const state = screen.getByLabelText(/State/i);
+  expect(state).toBeInTheDocument();
 });
